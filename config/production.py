9e8478071955e8df6193019ac20d1fec90de1cd4
chanhoobangN <chanhoobang@naver.com>
2024-05-21 13:10:23 +0900

    mysql 적용

--- config/production.py
+++ config/production.py
@@ -3,6 +3,7 @@ from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://test:1@localhost:3306/flask_db"
 
 SECRET_KEY=b'\xb1\x15\x83\x906\xd4\x9c\x8e\x07\xa7\x9eF\xc8|-\x94'
 
